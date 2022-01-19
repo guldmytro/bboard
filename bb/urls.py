749315b59a80a5a6da050b1f8fd4f3841bcc43a2
Dmytro Gul <guldmytro@gmail.com>
2022-01-19 16:14:58 +0200

    Media Root configuration fixed

--- bb/urls.py
+++ bb/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-import settings
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
